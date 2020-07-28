d6a08713385dac7d2eea2c8eaa4934998c4d8a9c
shota <shota@precure.tv>
2020-07-28 11:48:03 +0900

    スクリーン作成

---
+++ screen.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>すなっくプリキュア タイトルジェネレータ</title>
+        <link rel="stylesheet" href="./common/css/style.css">
+        <script type="text/javascript" src="//webfont.fontplus.jp/accessor/script/fontplus.js?Zxms2-UqCwA%3D&box=Go~A~tQ3zN8%3D&aa=1&ab=2" charset="utf-8"></script>
+    </head>
+    <body class="screen-body">
+        <div class="screen-outer">
+            <div class="screen-inner">
+                <div id="left-top">
+                    <div id="song-series" class="screen-title"></div>
+                    <div id="song-album" class="screen-title"></div>
+                    </div>
+                <div id="left-bottom">
+                    <div id="song-title" class="screen-title"></div>
+                    <div id="song-artist" class="screen-title"></div>
+                </div>
+                <div id="right-top">
+                    <div id="dj-current" class="screen-title"></div>
+                    <div id="dj-next" class="screen-title"></div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
