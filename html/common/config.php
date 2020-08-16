d1d9219a4421c4d8cc6f830e04fae4f642877af1
shota <shota@precure.tv>
2020-08-16 17:19:26 +0900

    セッション寿命

--- html/common/config.php
+++ html/common/config.php
@@ -4,7 +4,7 @@
 require_once __DIR__ . '/conn.php';
 
 // セッション寿命(秒)
-define('SESSION_LIFETIME', 1800);
+define('SESSION_LIFETIME', 3600 * 7);
 
 // DJリスト
 define('DJ_LIST', array('プリキュアおじさん', '100-200', '祥太'));
