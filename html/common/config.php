fad5b9e74e0950d4c547ef78238efc52054792dc
shota <shota@precure.tv>
2020-08-16 12:58:05 +0900

    キャラソン追加

--- html/common/config.php
+++ html/common/config.php
@@ -13,4 +13,5 @@ define('DJ_LIST', array('プリキュアおじさん', '100-200', '祥太'));
 define('CORNER_LIST', array(
     'snack200816_summer' => '夏っぽい曲増量中',
     'snack200816_request' => 'リクエスト強化中',
+    'snack200816_charasong' => 'キャラソンスペシャル',
 ));
