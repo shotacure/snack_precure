df8bf5055a70f4156d281913c86d9e90118f2e6f
shota <shota@precure.tv>
2020-12-12 23:33:40 +0900

    コーナー、ロゴ

--- html/common/config.php
+++ html/common/config.php
@@ -13,4 +13,5 @@ define('DJ_LIST', array('プリキュアおじさん', '100-200', '祥太'));
 define('CORNER_LIST', array(
     'snack201213_precure5' => '夢と希望のプリキュア5特集',
     'snack200816_request' => 'リクエスト強化中',
+    'snack201213_auction' => 'お店を応援！オークション',
 ));
